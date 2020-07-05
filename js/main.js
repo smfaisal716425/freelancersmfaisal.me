5548105cf0306caff87dad075d177de4c581c867
S.M. Faisal Mahmud <65438468+smfaisal716425@users.noreply.github.com>
2020-07-05 22:02:20 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*=======================*/
+/*Alert Danger*/
+/*=======================*/
+$('#error-alert').on("click", function(){
+  $('.alert-danger').show('slow');
+});
+
+
+/*=======================*/
+/*Scrolling Carousel for index*/
+/*=======================*/
+$(function(){
+  $(".g-scrolling-carousel .items").gScrollingCarousel();
+});
